3113264650e4e584f355646734ff98d64897db2d
Sarapuce <alexandre.herve97@gmail.com>
2026-08-21 16:37:03 +0200

    feat(): config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://sarapuce.fr",
   vite: {
     plugins: [tailwindcss()],
   },
